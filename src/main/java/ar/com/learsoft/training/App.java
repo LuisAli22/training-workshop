804e4d0a9fe4aa963d63c7926b54136d5499a074
LuisAli22 <luisali@fi.uba.ar>
2021-10-07 23:32:14 -0300

    Ok Matias presente

--- src/main/java/ar/com/learsoft/training/App.java
+++ src/main/java/ar/com/learsoft/training/App.java
@@ -6,5 +6,6 @@ public class App
     {
         System.out.println( "Bienvenido equipo!!" );
         System.out.println("Matias, Presente.");
+        System.out.println( "Ok Matias......!!" );
     }
 }
